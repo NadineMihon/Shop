6eb61cf09dd8019193ebe4ef91a42c7c679c5c64
Nadezhda Mikhonova <mihon.nadin@gmail.com>
2025-07-23 16:01:43 +0300

    feat(header): add dropdown menu for shop section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,38 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { CartAndFavoritesContext } from "../../context/cartAndFavoritesContext";
 import { PageContext } from "../../context/pageContext";
 
 export const Header = () => {
     const { countInBasket, countInFavorites } = useContext(CartAndFavoritesContext);
-    const { setPage } = useContext(PageContext);
+    const { page, setPage } = useContext(PageContext);
+
+    const [openDropdown, setOpenDropdown] = useState(null);
+
+    const menuItems = {
+        shop: ['Shop', 'Wishlist', 'Cart'],
+        pages: ['About', 'FAQ', 'My Profile']
+    };
+
+    const toggleDropdown = (menuName) => {
+        setOpenDropdown(prev => prev === menuName ? null : menuName);
+    };
+
+    const handleItemClick = (e, item) => {
+        e.stopPropagation();
+        setPage(item.toLowerCase());
+        setOpenDropdown(null);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (!(e.target).closest('.menu-item')) {
+                setOpenDropdown(null);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
     
     return (
         <header className="header">
@@ -27,12 +55,39 @@ export const Header = () => {
                             <img src="/icons/arrow-pink.svg" alt="arrow" className="arrow-hover" />
                         </div>
                     </div>
-                    <div className="menu-item active" onClick={() => setPage('shop')}>
-                        Shop
-                        <div className="arrow">
-                            <img src="/icons/arrow.svg" alt="arrow" className="arrow-default" />
-                            <img src="/icons/arrow-pink.svg" alt="arrow" className="arrow-hover" />
-                        </div>
+                    <div 
+                        className="menu-item active" 
+                        onClick={() => toggleDropdown('shop')}
+                        aria-haspopup="true"
+                        aria-expanded={openDropdown === 'shop'}
+                    >
+                            Shop
+                            <div className="arrow">
+                                <img src="/icons/arrow.svg" alt="arrow" className="arrow-default" />
+                                <img 
+                                    src="/icons/arrow-pink.svg" 
+                                    alt="arrow" 
+                                    className={`arrow-hover ${openDropdown === 'shop' ? 'rotated' : '' }`}
+                                />
+                            </div>
+                            {
+                                openDropdown === 'shop' && (
+                                    <div className="dropdown-menu">
+                                        {
+                                            menuItems.shop.map(item => (
+                                                <div
+                                                    key={item}
+                                                    className={`dropdown-item ${page === item.toLocaleLowerCase() 
+                                                        ? 'active' : ''}`}
+                                                    onClick={(e) => handleItemClick(e, item)}
+                                                >
+                                                        {item}
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                )
+                            }
                     </div>
                     <div className="menu-item">Blog</div>
                     <div className="menu-item">Contact</div>
